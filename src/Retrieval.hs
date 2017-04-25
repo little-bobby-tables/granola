154fbf2a456224830379a28b8b43d537c3f415b8
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-04-25 14:55:21 +0700

    search by multiple prefixes
    e.g. "awe ta" -> "awesome tag"

--- src/Retrieval.hs
+++ src/Retrieval.hs
@@ -1,24 +1,29 @@
-{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE LambdaCase, FlexibleContexts #-}
 
 module Retrieval where
-  import Strings (UTF8BString, (@+), utf8ToString)
+  import Strings (UTF8BString, (@+), utf8ToString, stringToUtf8)
 
-  import Database.Redis (Connection, RedisCtx, runRedis,
-    zrevrangebyscoreWithscoresLimit)
+  import Data.List (intercalate)
+
+  import Database.Redis (Connection, Aggregate(Max), runRedis,
+    zinterstore, expire, zrevrangebyscoreWithscoresLimit)
 
   search :: Connection -> UTF8BString -> IO [(String, Int)]
   search redis query =
     let terms = words (utf8ToString query)
     in case (length terms) of
-      1 -> searchByKeyWithLimit redis ("search:" @+ query) 10
-      _ -> return []
-
-  searchByKeyWithLimit :: Connection -> UTF8BString -> Integer -> IO [(String, Int)]
-  searchByKeyWithLimit redis key count =
-    runRedis redis $ highestRanking >>= \case
-      Right tags -> return $ map (\(tag, score) ->
-        (utf8ToString tag, round score)) tags
-      Left _ -> return []
-    where
-      highestRanking = searchWithScoreAboveZero 0 count
-      searchWithScoreAboveZero = zrevrangebyscoreWithscoresLimit key (1 / 0) 1
+      1 ->
+        runRedis redis $ getTopTagsAt ("search:" @+ query) 10
+      _ ->
+        let intersectionKey = stringToUtf8 (intercalate "|" terms)
+            termKeys = map (("search:" @+) . stringToUtf8) terms
+        in runRedis redis $
+          zinterstore intersectionKey termKeys Max
+          >> expire intersectionKey 60
+          >> getTopTagsAt intersectionKey 10
+      where
+        getTopTagsAt key count = withScoreAboveZero key 0 count >>= \case
+          Right tags -> return $ (\(tag, score) ->
+            (utf8ToString tag, round score)) <$> tags
+          Left _ -> return []
+        withScoreAboveZero key = zrevrangebyscoreWithscoresLimit key (1 / 0) 1
