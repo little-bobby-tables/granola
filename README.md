76072c87ab096e9090f5f5278b271c710614bd8c
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-04-26 22:54:28 +0700

    complete integration specs

--- README.md
+++ README.md
@@ -7,6 +7,14 @@ A Redis-based ordered autocomplete service.
 * [stack](https://github.com/commercialhaskell/stack)
 * Redis
 
+## Documentation
+
+Granola is intended to be used as an HTTP microservice.
+Its API consists of two endpoints (*\index* and *\search*).
+
+You can find detailed usage examples in the
+[integration spec](https://github.com/little-bobby-tables/granola/blob/master/test/IntegrationSpec.hs).
+
 ## Testing
 
 **Warning**: the tests will attempt to connect to Redis at localhost:6379 and
--- test/IntegrationSpec.hs
+++ test/IntegrationSpec.hs
@@ -6,6 +6,8 @@ module IntegrationSpec where
 
   import Utils (redis, flushdb)
 
+  import Strings (stringToUtf8)
+
   import Server (app)
 
   main :: IO ()
@@ -13,24 +15,49 @@ module IntegrationSpec where
 
   spec :: Spec
   spec = before_ flushdb $ with (app <$> redis) $ do
-    describe "indexing" $ do
-      context "adding a new tag" $ do
-        it "increments its score" $ do
-          get "/search?q=tag" `shouldRespondWith` "[]"
-          get "/index?add=[\"tag\"]" `shouldRespondWith` 200
-          get "/search?q=tag" `shouldRespondWith` "[[\"tag\",1]]"
-              { matchStatus = 200
-              , matchHeaders = ["Content-Type" <:> "application/json;charset=utf-8"] }
+    describe "adding a new tag" $
+      it "increments its score" $ do
+        get "/search?q=tag" `shouldRespondWith` "[]"
+        get "/index?add=[\"tag\"]" `shouldRespondWith` 200
+        get "/search?q=tag" `shouldRespondWith` "[[\"tag\",1]]"
 
-      context "removing an existing tag" $ do
-        it "decrements its score" $ do
-          replicateM_ 2 $ get "/index?add=[\"tag\"]"
-          get "/search?q=tag" `shouldRespondWith` "[[\"tag\",2]]"
-          get "/index?remove=[\"tag\"]" `shouldRespondWith` 200
-          get "/search?q=tag" `shouldRespondWith` "[[\"tag\",1]]"
+    describe "removing an existing tag" $ do
+      it "decrements its score" $ do
+        replicateM_ 2 $ get "/index?add=[\"tag\"]"
+        get "/search?q=tag" `shouldRespondWith` "[[\"tag\",2]]"
+        get "/index?remove=[\"tag\"]" `shouldRespondWith` 200
+        get "/search?q=tag" `shouldRespondWith` "[[\"tag\",1]]"
 
-        it "hides the tag once its score reaches 0" $ do
-          replicateM_ 2 $ get "/index?add=[\"tag\"]"
-          get "/search?q=tag" `shouldRespondWith` "[[\"tag\",2]]"
-          replicateM_ 2 $ get "/index?remove=[\"tag\"]" `shouldRespondWith` 200
-          get "/search?q=tag" `shouldRespondWith` "[]"
+      it "hides the tag once its score reaches 0" $ do
+        replicateM_ 2 $ get "/index?add=[\"tag\"]"
+        get "/search?q=tag" `shouldRespondWith` "[[\"tag\",2]]"
+        replicateM_ 2 $ get "/index?remove=[\"tag\"]" `shouldRespondWith` 200
+        get "/search?q=tag" `shouldRespondWith` "[]"
+
+    describe "searching" $ do
+      it "returns the results ordered by score" $ do
+        replicateM_ 2 $ get "/index?add=[\"a tag\"]"
+        replicateM_ 5 $ get "/index?add=[\"second tag\"]"
+        replicateM_ 3 $ get "/index?add=[\"third tag\"]"
+        get "/search?q=tag" `shouldRespondWith`
+          "[[\"second tag\",5],[\"third tag\",3],[\"a tag\",2]]"
+            { matchStatus = 200
+            , matchHeaders = ["Content-Type" <:> "application/json;charset=utf-8"] }
+
+      it "can search by multiple prefixes" $
+        get "/index?add=[\"pearl\"\
+          \,\"revengeful\"\
+          \,\"terrifying renegade pearl\"\
+          \,\"rebellious peridot\"\
+          \,\"restrained sapphire\"\
+          \,\"crying pear\"]" >>
+        get "/search?q=re pe" `shouldRespondWith`
+          "[[\"terrifying renegade pearl\",1]\
+          \,[\"rebellious peridot\",1]]"
+
+    describe "encoding" $
+      it "properly indexes unicode tags" $
+        get (stringToUtf8 "/index?add=[\"юникод\",\"юникод юникод\"]") >>
+        get (stringToUtf8 "/search?q=юни") `shouldRespondWith`
+          "[[\"юникод юникод\",1]\
+          \,[\"юникод\",1]]"
